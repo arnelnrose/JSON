eefe8912cd1f0f5f417ec3b0f31c23f5a852c61f
Esor <155610717+arnelnrose@users.noreply.github.com>
2024-05-14 00:29:37 +0800

    Delete 2-JsonStructureArrays.js

--- 2-JsonStructureArrays.js
+++
@@ -1,9 +0,0 @@
-// A JSON can also have arrays as its value.
-
-let person = {
-    firstName: "Arnel",
-    lastName: "Rose",
-    age: 43,
-    hobbies:["gaming", "eating", "sleeping"]
-};
-console.log(person);
