ef0c19f71f53cbb0cc1faa7bd809ba29e7bc9655
Esor <155610717+arnelnrose@users.noreply.github.com>
2024-05-14 00:32:20 +0800

    Delete 9-JSONParsing.js

--- 9-JSONParsing.js
+++
@@ -1,9 +0,0 @@
-/**
-    You can convert valid Strings into JSON
-    by using the JSON.parse() method.
- */
-
-let strPerson = `{"firstName": "Arnel", "lastName": "Rose", "age": 42}`;
-
-let person = JSON.parse(strPerson);
-console.log(person);
